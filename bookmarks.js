f51fa65fd91d7618f175cb0a6883dfb21f021fba
wulangsir <44461585+wulangsir@users.noreply.github.com>
2026-03-11 20:02:11 +0800

    浏览器书签管理器

---
+++ bookmarks.js
@@ -0,0 +1,119 @@
+// 书签数据 (可被 script 标签加载)
+window.bookmarksData = [
+  {
+    "title": "Dribbble",
+    "url": "https://dribbble.com",
+    "category": "设计资源",
+    "id": "1773214134693-8tln305w",
+    "tags": [],
+    "favorite": false
+  },
+  {
+    "title": "无风不起浪_你的微博_微博",
+    "url": "https://weibo.com/u/6420410379",
+    "category": "社交媒体",
+    "id": "1773214134693-gvxc8ejp",
+    "tags": [],
+    "favorite": false
+  },
+  {
+    "title": "Java中获取当前路径的几种方法总结-eolink官网",
+    "url": "https://www.eolink.com/news/post/75639.html",
+    "category": "新闻资讯",
+    "id": "1773214134693-ysfobl5z",
+    "tags": [],
+    "favorite": false
+  },
+  {
+    "title": "矢量图库_素材中国_素材天下",
+    "url": "http://www.sccnn.com/shiliangtuku/",
+    "category": "新闻资讯",
+    "id": "1773214134693-atgupbke",
+    "tags": [],
+    "favorite": false
+  },
+  {
+    "title": "Code School",
+    "url": "https://www.pluralsight.com/codeschool",
+    "category": "学习教育",
+    "id": "1773214134693-sx6gb0tv",
+    "tags": [],
+    "favorite": false
+  },
+  {
+    "title": "Playground | OpenRouter",
+    "url": "https://openrouter.ai/playground?models=openai/gpt-3.5-turbo,gryphe/mythomax-l2-13b,mistralai/mistral-7b-instruct",
+    "category": "AI / 机器学习",
+    "id": "1773214134693-uxy33rta",
+    "tags": [],
+    "favorite": false
+  },
+  {
+    "title": "arXiv",
+    "url": "https://arxiv.org",
+    "category": "AI / 机器学习",
+    "id": "1773214134693-w8gp89hk",
+    "tags": [],
+    "favorite": false
+  },
+  {
+    "id": "1773227719843-p5xw96",
+    "url": "https://wulangsir.github.io/",
+    "title": "无风不起浪@你",
+    "category": "其它/未分类",
+    "tags": [
+      "boke",
+      "前端工程师",
+      "美工"
+    ],
+    "favorite": true,
+    "createdAt": 1773227719843
+  },
+  {
+    "id": "1773227783234-g3z357",
+    "url": "https://wulangsir.github.io/kaoji.html",
+    "title": "英才考级 · 多文件多表校验（wulangsir+无浪科技）",
+    "category": "工具网站",
+    "tags": [
+      "wulangsir",
+      "无浪科技"
+    ],
+    "favorite": true,
+    "createdAt": 1773227783234
+  },
+  {
+    "id": "1773227866011-b4hxxj",
+    "url": "https://wulangsir.github.io/zhaopian.html",
+    "title": "excel表格姓名匹配照片匹配",
+    "category": "工具网站",
+    "tags": [
+      "图片",
+      "excel"
+    ],
+    "favorite": true,
+    "createdAt": 1773227866011
+  },
+  {
+    "id": "1773227907284-kylxii",
+    "url": "https://wulangsir.github.io/index/",
+    "title": "作者的博客",
+    "category": "其它/未分类",
+    "tags": [
+      "博客",
+      "作者"
+    ],
+    "favorite": true,
+    "createdAt": 1773227907284
+  },
+  {
+    "id": "1773227948161-wgmzht",
+    "url": "https://wulangsir.github.io/home/",
+    "title": "无浪网静态页面",
+    "category": "工具网站",
+    "tags": [
+      "无浪网"
+    ],
+    "favorite": true,
+    "createdAt": 1773227948161
+  }
+];
